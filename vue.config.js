239d5031a36f22caedfb44d38f273f51f023adb0
xiaodu <you@example.com>
2020-07-27 22:38:32 +0800

    项目打包-加载体积优化

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-  lintOnSave: false
+  // 保存时不做代码的风格校验
+  lintOnSave: false,
+  // 把一些第三方包排除在外，不去打包
+  // vue-cli -> webpack打包工具
+  configureWebpack:{
+    externals:{
+      // key(包的名字):value(通过script标签引入后，暴露在全局的变量名)
+      echarts: 'echarts'
+    }
+  }
 }
